4fb86710811da67308653db2c1c6be4c5d8c2071
Mikko Wrightson <wonmotlow@gmail.com>
2020-10-26 23:37:14 +0200

    props

--- 1/part1/src/index.js
+++ 1/part1/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
@@ -12,16 +12,14 @@ const Hello = () => {
 
 
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+  const nimi = 'Pekka'
+  const ika = 10
 
   return (
     <div>
       <h1>Greetings!</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
     </div>
   )
 }
