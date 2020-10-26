d3074856c59eca3592b402dcfbb1e49e85b18b1a
Mikko Wrightson <wonmotlow@gmail.com>
2020-10-26 23:19:45 +0200

    dynamic content

--- 1/part1/src/index.js
+++ 1/part1/src/index.js
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  console.log('Hello from komponentti')
+  const now = new Date()
+  const a = 10
+  const b = 20
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   )
 }
-
 ReactDOM.render(<App />, document.getElementById('root'))
