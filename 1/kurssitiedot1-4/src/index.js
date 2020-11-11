eb6c92bf2c2c1b69558d42ed2e1da67cd9491d38
Mikko Wrightson <wonmotlow@gmail.com>
2020-11-11 16:17:51 +0200

    Add .gitignore

--- 1/kurssitiedot1-4/src/index.js
+++ 1/kurssitiedot1-4/src/index.js
@@ -6,25 +6,27 @@ const App = () => {
 
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total part1={part1} part2={part2} part3={part3}/>
+      <Content part1={parts[]} part2={parts[]} part3={parts[]}/>
+      <Total part1={parts[1]} part2={parts[1]} part3={parts[1]}/>
     </div>
   )
 
